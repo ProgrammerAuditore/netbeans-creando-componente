154857609ec43dbc580d8f7f2f4c51f2c8f10316
ProgrammerAuditore <b4rce3l0n4@gmail.com>
2020-11-11 00:13:44 -0600

    ****** Anexando
    Este es el primer ejemplo del componente v1.0Alpha

--- JPanelImagen/src/jpanelimagen/JPanelImagen.java
+++ JPanelImagen/src/jpanelimagen/JPanelImagen.java
@@ -10,6 +10,29 @@ import javax.swing.JPanel;
 
 public class JPanelImagen extends JPanel implements Serializable{
     
-   
+    private File rutaImagen;
+    
+    // Método constructor
+    public JPanelImagen(){
+
+    }
+
+    public File getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(File rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); 
+        if( rutaImagen.exists() && rutaImagen != null ){
+            ImageIcon ic = new ImageIcon( rutaImagen.getAbsolutePath() );
+            ImageIcon icOriginal = new ImageIcon( ic.getImage().getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH) );
+            g.drawImage(icOriginal.getImage() , 0, 0, null);
+        }
+    }
            
 }
