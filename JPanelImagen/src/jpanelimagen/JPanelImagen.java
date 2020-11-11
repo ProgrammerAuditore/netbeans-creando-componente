6f72746cc26e1b8f05ad791330f376ba6d6149cc
ProgrammerAuditore <b4rce3l0n4@gmail.com>
2020-11-10 23:54:58 -0600

    Inicializando repositorio...
    Revert "Initial commit"
    
    This reverts commit a75c58681c0e5983508b122712aa3465939fc748.
    
    Inicializando proyecto...

---
+++ JPanelImagen/src/jpanelimagen/JPanelImagen.java
@@ -0,0 +1,15 @@
+package jpanelimagen;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JPanelImagen extends JPanel implements Serializable{
+    
+   
+           
+}
